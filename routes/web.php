7aeeafcdf3b2f79ac028f5ffc13d4f8e04d467dc
Gaby10x <gabrielayupangui2002@gmail.com>
2024-01-27 18:37:32 -0500

    HomeDinamico

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [App\Http\Controllers\WelcomeController::class, 'mostrarDatos'])->name('welcome');
 
 Route::middleware([
     'auth:sanctum',
