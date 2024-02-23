169445b0986023acc51e48f00a3b72d8f4c86808
Gaby10x <gabrielayupangui2002@gmail.com>
2024-02-23 09:07:30 -0500

    APIS

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocenteController;
+use App\Http\Controllers\AreaConocimientoController;
+use App\Http\Controllers\MateriaController;
+use App\Http\Controllers\NrcController;
+use App\Http\Controllers\SemestreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+Route::apiResource("docente", DocenteController::class);
+Route::apiResource("area", AreaConocimientoController::class);
+Route::apiResource("materia", MateriaController::class);
+Route::apiResource("nrc", NrcController::class);
+Route::apiResource("semestre", SemestreController::class);
